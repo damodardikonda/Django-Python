8469af232fdf865f071cef3be05b9e0ab023b495
unknown <damodar2dikonda@gmail.com>
2020-07-09 18:27:53 +0530

    Django

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,8 +1,14 @@
 
 from django.urls import path
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),# we cant pass actually name of class .. we have to pass an method()
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),#1 parameter is an path name.
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    #<int:pk> shows that gives primarty key but it will always an integer not string or else..
     path('about/', views.about, name='blog-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import( ListView,
+                                  DetailView,
+                                  CreateView,#if user want to create view
+)#this is used for creating list and DetailView is used for taking each post detail
 from django.http import HttpResponse
 from .models import Post
 # Create your views here.
@@ -9,6 +13,27 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model=Post #remember we are providing an class name in to that models
+    template_name='blog/home.html'#<app>/<model>_<viewType>.html
+    context_object_name='posts'
+    ordering=['-date_posted']# it will place latest post on the top
+
+class PostCreateView(CreateView):
+    model=Post
+    fields=['title','content']# we want that two fields .
+
+# this is for cretingnew post through Websites
+
+    def form_valid(self,form):         # overriding form valid() method
+         form.instance.author=self.request.user           # it  sets the current user tothat instance author
+         return super().form_valid(form)             # it is validating that parent form
+
+
+class PostDetailView(DetailView):# it for the dailed view for post
+    model=Post
+
 
 
 def about(request):
