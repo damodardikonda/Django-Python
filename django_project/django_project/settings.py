bfd4bcfec85c3a9b68503e346a476a4c7f489fb1
unknown <damodar2dikonda@gmail.com>
2020-07-13 22:25:49 +0530

    Django

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -129,3 +129,12 @@ CRISPY_TEMPLATE_PACK= 'bootstrap4'
 
 LOGIN_REDIRECT_URL='blog-home' # if username and password of login page is corrected then it goes to blog-home page..
 LOGIN_URL='Login'# if we directly search for profilr then it will send to login page
+
+
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -25,6 +25,20 @@ urlpatterns = [
     path('profile/', user_views.profile,name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name='Login'),# if we does not pass an template name then it gives error 'templateDoesNotExist'because it does not support templates
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='Logout'),# if we does not pass an template name then it gives error 'templateDoesNotExist'because it does not support templates
+    path('password_reset/',
+       auth_views.PasswordResetView.as_view(template_name='users/reset_password.html'),
+       name='password_reset'),
+
+    path('password_reset/done/',
+       auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+       name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',
+       auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+       name='password_reset_confirm'),
+    path('password_reset_complete/<uidb64>/<token>/',
+       auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+       name='password_reset_complete'),
+
     path('', include('blog.urls')),
 
 ]
